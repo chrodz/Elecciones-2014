b38f33c5f29fb92c55f6e1e01bbc4af0af5f7f1c
Pierre Chavez <pierre.chavez@ec.pe>
2014-08-22 12:57:31 -0500

    Huacho Paita

---
+++ descarga.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chrod
+ * Date: 22/08/14
+ * Time: 12:27 PM
+ */
+
+$departamentos = array('');
+
+$ruta = 'http://consultamiembrodemesa.onpe.gob.pe/pdf/LIMA/LIMA/LINCE/LINCE.pdf';
